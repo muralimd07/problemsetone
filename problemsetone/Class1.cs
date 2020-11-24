3139707fc417bd22eef5b499ae3a5ce8fa152692
muralimd07 <60916429+muralimd07@users.noreply.github.com>
2020-11-24 07:37:28 -0800

    initialcommit
    test

---
+++ problemsetone/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problemsetone
+{
+    public class Class1
+    {
+      
+        #region sum,difference,and products
+        public int sum(int a,int b)
+        {
+
+            return a + b;
+           
+        }
+        #endregion
+    }
+}
